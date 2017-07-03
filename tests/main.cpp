e22d071ff1351a3e5f7ba6374656f4d57fd4c401
trent33 <clunietp@gmail.com>
2017-07-03 09:59:47 -0400

    Initial commit

---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#include "../value_ptr.hpp"
+
+BOOST_AUTO_TEST_CASE( test ) {
+	BOOST_CHECK( true );
+}
---
+++ value_ptr.hpp
@@ -0,0 +1,12 @@
+#ifndef _SMART_PTR_VALUE_PTR_
+#define _SMART_PTR_VALUE_PTR_
+
+namespace smart_ptr {
+
+	struct value_ptr {
+
+	};	// value_ptr
+
+}	// smart_ptr ns
+#endif // !_SMART_PTR_VALUE_PTR_
+
